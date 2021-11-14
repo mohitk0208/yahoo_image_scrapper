23f6559843b654ab0e5626987ba5cb1971fbd07d
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-11-14 12:37:22 +0530

    use spaces

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const rp = require("request-promise");
 const cheerio = require("cheerio");
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -14,87 +13,88 @@ app.use(express.json());
  * @return {object}
  */
 const extract_info = (url) => {
-	const back = url.match(/(?<=back\=).*/)[0];
-	const imgurl = back.match(/(?<=imgurl\=).*?(?=\&)/)[0];
-	const rurl = back.match(/(?<=rurl\=).*?(?=\&)/)[0];
-	const height = back.match(/(?<=h\=).*?(?=\&)/)[0];
-	const width = back.match(/(?<=w\=).*?(?=\&)/)[0];
-	const size = back.match(/(?<=size\=).*?(?=\&)/)[0];
-	const name = back.match(/(?<=tt\=).*?(?=\&)/)[0].replace(/\+/g, " ");
-	const info = {
-		imgurl: "https://" + imgurl,
-		rurl,
-		height,
-		width,
-		size,
-		name,
-	};
-	return info;
+  const back = url.match(/(?<=back\=).*/)[0];
+  const imgurl = back.match(/(?<=imgurl\=).*?(?=\&)/)[0];
+  const rurl = back.match(/(?<=rurl\=).*?(?=\&)/)[0];
+  const height = back.match(/(?<=h\=).*?(?=\&)/)[0];
+  const width = back.match(/(?<=w\=).*?(?=\&)/)[0];
+  const size = back.match(/(?<=size\=).*?(?=\&)/)[0];
+  const name = back.match(/(?<=tt\=).*?(?=\&)/)[0].replace(/\+/g, " ");
+  const info = {
+    imgurl: "https://" + imgurl,
+    rurl,
+    height,
+    width,
+    size,
+    name,
+  };
+  return info;
 };
 
 const getImages = async (query) => {
-	const html = await rp(
-		"https://in.images.search.yahoo.com/search/images?p=" + query
-	).catch((error) => console.log("Error : ", error));
+  const html = await rp(
+    "https://in.images.search.yahoo.com/search/images?p=" + query
+  ).catch((error) => console.log("Error : ", error));
 
-	const $ = cheerio.load(html);
+  const $ = cheerio.load(html);
 
-	const links = $("#sres li a");
+  const links = $("#sres li a");
 
-	const urls = links
-		.map((i, e) => {
-			const url = decodeURIComponent($(e).prop("href"));
-			extract_info(url);
-			const preview = $(e).children("img").prop("data-src");
-			const data = {
-				...extract_info(url),
-				preview,
-			};
-			return data;
-		})
-		.get();
+  const urls = links
+    .map((i, e) => {
+      const url = decodeURIComponent($(e).prop("href"));
+      extract_info(url);
+      const preview = $(e).children("img").prop("data-src");
+      const data = {
+        ...extract_info(url),
+        preview,
+      };
+      return data;
+    })
+    .get();
 
-	return Promise.all(urls);
+  return Promise.all(urls);
 };
 
 app.get("/", (req, res) => {
-	res.status(200);
-	res.json({ message: "send requests at /{query}" });
+  res.status(200);
+  res.json({ message: "send requests at /{query}" });
 });
 
 const response = {
-	query: "",
-	result: [
-		{
-			imgurl: "",
-			rurl: "",
-			height: "",
-			width: "",
-			size: "",
-			name: "",
-			preview: "",
-		},
-	],
+  query: "",
+  result: [
+    {
+      imgurl: "",
+      rurl: "",
+      height: "",
+      width: "",
+      size: "",
+      name: "",
+      preview: "",
+    },
+  ],
 };
 
 app.get("/testsejvnajergu423bar453knmf423oi8r789egunaljnv", (req, res) => {
-	res.status(200).json(response);
+  res.status(200).json(response);
 });
 
 app.get("/api/getimage/:query", async (req, res) => {
-	const query = req.params.query;
-	try {
-		const result = await getImages(query);
-		res.status(200).json({ query, result });
-		console.log(`Successfullly scrapped data (${query})`);
-	} catch (err) {
-		console.log("ERROR :", err);
-	}
+  const query = req.params.query;
+
+  try {
+    const result = await getImages(query);
+    res.status(200).json({ query, result });
+    console.log(`Successfully scrapped data (${query})`);
+  } catch (err) {
+    console.log("ERROR :", err);
+  }
 
 });
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-	port = 8000;
+  port = 8000;
 }
-app.listen(port, () => console.log("Server started"));
+app.listen(port, () => console.log(`Server started at http://localhost:${port} `));
